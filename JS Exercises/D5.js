1b880a530ff52246cd24c4504c911adaf3d95807
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-06-06 23:12:31 +0200

    adds javascript solutions

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -10,69 +10,220 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
+const pets = ["dog", "cat", "hamster", "redfish"];
+
+for (i = 0; i < pets.length; i++) {
+  console.log(pets[i], " ");
+}
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
 
+// pets.sort();
+const petsSorted = [...pets].sort();
+console.log(petsSorted);
+
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
+const petsReverse = [...pets].sort();
+petsReverse.reverse();
+
+for (i = 0; i < pets.length; i++) {
+  console.log(petsReverse[i], " ");
+}
+
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+
+const petsMove = [...pets];
+const move = function () {
+  petsMove.push(petsMove[0]);
+  petsMove.shift();
+};
+
+move();
+
+console.log(petsMove);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
 const cars = [
   {
-    brand: 'Ford',
-    model: 'Fiesta',
-    color: 'red',
-    trims: ['titanium', 'st', 'active'],
+    brand: "Ford",
+    model: "Fiesta",
+    color: "red",
+    trims: ["titanium", "st", "active"],
   },
   {
-    brand: 'Peugeot',
-    model: '208',
-    color: 'blue',
-    trims: ['allure', 'GT'],
+    brand: "Peugeot",
+    model: "208",
+    color: "blue",
+    trims: ["allure", "GT"],
   },
   {
-    brand: 'Volkswagen',
-    model: 'Polo',
-    color: 'black',
-    trims: ['life', 'style', 'r-line'],
+    brand: "Volkswagen",
+    model: "Polo",
+    color: "black",
+    trims: ["life", "style", "r-line"],
   },
-]
+];
+
+cars[0].licensePlate = "AB123CD";
+cars[1].licensePlate = "AB456CD";
+cars[2].licensePlate = "AB789CD";
+
+console.log(cars[0].licensePlate, cars[1].licensePlate, cars[2].licensePlate);
+
+// sopra l'ho fatto a mano con le conoscenze acquisite fino ad ora nel corso, qui sotto invece metodo sfruttando google e siti come stack overflow/w3s
+
+const carsFunction = [...cars];
+const aggiungiLicensePlate = (arr, ...licensePlates) => {
+  arr.forEach((obj, index) => {
+    obj.licensePlate = licensePlates[index];
+  });
+};
+
+aggiungiLicensePlate(carsFunction, "SB324SD", "SB234AS", "SB394ZJ");
+console.log(
+  carsFunction[0].licensePlate,
+  carsFunction[1].licensePlate,
+  carsFunction[2].licensePlate
+);
+
+// ho visto che in caso si può anche far scegliere la chiave aggiungendo key (variabile) nelle prorietà e poi scrivendo obj[key] alla riga 87
 
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
 
+cars.push({
+  brand: "Fiat",
+  model: "Punto",
+  color: "grey",
+  trims: ["1", "2", "3"],
+  licensePlate: "AB012HJ",
+});
+console.log(cars[3]);
+
+for (i = 0; i < cars.length; i++) {
+  cars[i].trims.pop([cars[i].trims.length - 1]);
+}
+console.log(cars[0].trims, cars[1].trims, cars[2].trims, cars[3].trims);
+
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
 */
-const justTrims = []
+const justTrims = [];
+
+for (i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0]);
+}
+
+console.log(justTrims);
 
 /* ESERCIZIO 8
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
 
+for (i = 0; i < cars.length; i++) {
+  if (cars[i].color.startsWith("b")) {
+    console.log("Fizz");
+  } else {
+    console.log("Buzz");
+  }
+}
+
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
-]
+];
+
+let x = 0;
+while (numericArray[x] !== 32) {
+  console.log(numericArray[x]);
+  x++;
+}
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
     dell'alfabeto italiano.
     es. [f, b, e] --> [6, 2, 5]
 */
-const charactersArray = ['g', 'n', 'u', 'z', 'd']
+const charactersArray = ["g", "n", "u", "z", "d"];
+
+for (i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case "a":
+      console.log("1 ");
+      break;
+    case "b":
+      console.log("2 ");
+      break;
+    case "c":
+      console.log("3 ");
+      break;
+    case "d":
+      console.log("4 ");
+      break;
+    case "e":
+      console.log("5 ");
+      break;
+    case "f":
+      console.log("6 ");
+      break;
+    case "g":
+      console.log("7 ");
+      break;
+    case "h":
+      console.log("8 ");
+      break;
+    case "i":
+      console.log("9 ");
+      break;
+    case "l":
+      console.log("10 ");
+      break;
+    case "m":
+      console.log("11 ");
+      break;
+    case "n":
+      console.log("12 ");
+      break;
+    case "o":
+      console.log("13 ");
+      break;
+    case "p":
+      console.log("14 ");
+      break;
+    case "q":
+      console.log("15 ");
+      break;
+    case "r":
+      console.log("16 ");
+      break;
+    case "s":
+      console.log("17 ");
+      break;
+    case "t":
+      console.log("18 ");
+      break;
+    case "u":
+      console.log("19 ");
+      break;
+    case "v":
+      console.log("20 ");
+      break;
+    case "z":
+      console.log("21 ");
+      break;
+  }
+}
